b820a56250f9142aa6669866f83399ac89feb8be
Berat BAYRAM <berat.bayram@tubitak.gov.tr>
2024-07-09 15:13:04 +0300

    css'i sal artık

--- app/colored-table/page.tsx
+++ app/colored-table/page.tsx
@@ -3,57 +3,101 @@
 import { useState } from "react";
 import "./page.css";
 
-interface Props {
-  classes: [string, string, string];
-}
-
-const COLORS = ["green", "yellow", ""];
-
 interface CellProps {
-  defaultClassName: string;
+  className: string;
   setIslightGreen: (value: boolean) => void;
+  setIslightYellow: (value: boolean) => void;
+  setIslightBlue: (value: boolean) => void;
 }
 
-function Cell({ defaultClassName, setIslightGreen }: CellProps) {
-
+function Cell({
+  className,
+  setIslightGreen,
+  setIslightBlue,
+  setIslightYellow,
+}: CellProps) {
   function changeColor() {
-    setIslightGreen(defaultClassName === "green" ? true : false)
+    if (className.includes("green"))
+      setIslightGreen(className === "green" ? true : false);
+    else if (className.includes("yellow"))
+      setIslightYellow(className === "yellow" ? true : false);
+    else setIslightBlue(className === "" ? true : false);
   }
 
-  return <div onClick={changeColor} className={defaultClassName}></div>;
+  return <div onClick={changeColor} className={className}></div>;
 }
 
 interface RowProps {
   classes: [string, string, string];
   setIslightGreen: (value: boolean) => void;
+  setIslightYellow: (value: boolean) => void;
+  setIslightBlue: (value: boolean) => void;
 }
 
-function Row({ classes, setIslightGreen }: RowProps) {
+function Row({
+  classes,
+  setIslightGreen,
+  setIslightBlue,
+  setIslightYellow,
+}: RowProps) {
   // array destructing örneği
   // (classes[0] da kullanılabilir)
   const [classes1, classes2, classes3] = classes;
 
   return (
     <div className="row">
-      <Cell setIslightGreen={setIslightGreen} defaultClassName={classes1} />
-      <Cell setIslightGreen={setIslightGreen} defaultClassName={classes2} />
-      <Cell setIslightGreen={setIslightGreen} defaultClassName={classes3} />
+      <Cell
+        setIslightBlue={setIslightBlue}
+        setIslightYellow={setIslightYellow}
+        setIslightGreen={setIslightGreen}
+        className={classes1}
+      />
+      <Cell
+        setIslightBlue={setIslightBlue}
+        setIslightYellow={setIslightYellow}
+        setIslightGreen={setIslightGreen}
+        className={classes2}
+      />
+      <Cell
+        setIslightBlue={setIslightBlue}
+        setIslightYellow={setIslightYellow}
+        setIslightGreen={setIslightGreen}
+        className={classes3}
+      />
     </div>
   );
 }
 
 export default function Home() {
-
   const [islightGreen, setIslightGreen] = useState(false);
+  const [islightYellow, setIslightYellow] = useState(false);
+  const [islightBlue, setIslightBlue] = useState(false);
 
   const green = islightGreen ? "lightgreen" : "green";
+  const yellow = islightYellow ? "lightyellow" : "yellow";
+  const blue = islightBlue ? "lightblue" : "";
 
   return (
     <main>
       <div>
-        <Row setIslightGreen={setIslightGreen} classes={[green, "", ""]} />
-        <Row setIslightGreen={setIslightGreen} classes={["", "yellow", ""]} />
-        <Row setIslightGreen={setIslightGreen} classes={["", "", green]} />
+        <Row
+          setIslightBlue={setIslightBlue}
+          setIslightYellow={setIslightYellow}
+          setIslightGreen={setIslightGreen}
+          classes={[green, blue, blue]}
+        />
+        <Row
+          setIslightBlue={setIslightBlue}
+          setIslightYellow={setIslightYellow}
+          setIslightGreen={setIslightGreen}
+          classes={[blue, yellow, blue]}
+        />
+        <Row
+          setIslightBlue={setIslightBlue}
+          setIslightYellow={setIslightYellow}
+          setIslightGreen={setIslightGreen}
+          classes={[blue, blue, green]}
+        />
       </div>
     </main>
   );
