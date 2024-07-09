ae1f5d05e6aece72486f381a9e2eed884721d13b
Berat BAYRAM <berat.bayram@tubitak.gov.tr>
2024-07-09 15:58:45 +0300

    Form uncontrolled

--- app/form-controlled/page.tsx
+++ app/form-controlled/page.tsx
@@ -1,10 +1,21 @@
 "use client";
 import { useState } from "react";
 
+function isPasswordValid(password: string): boolean {
+  return (
+    password.length >= 6 &&
+    password
+      .split("")
+      .every((char) =>
+        ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].includes(char)
+      )
+  );
+}
+
 export default function App() {
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
-
+  
   function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
     setPassword(event.target.value);
   }
@@ -37,7 +48,11 @@ export default function App() {
           />
         </label>
         <br />
-        <input type="submit" value="Submit"  disabled/>
+        <input
+          type="submit"
+          value="Submit"
+          disabled={!isPasswordValid(password)}
+        />
       </form>
     </main>
   );
